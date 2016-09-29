2095c8965017a07a2320a46b487c5f0fc559ae63
Thibault Févry <ThibaultFevry@gmail.com>
2016-09-29 17:14:08 +0200

    Embellishments.

--- process.py
+++ process.py
@@ -1,20 +1,37 @@
+"""
+Processing data after it has been cleaned by transform_data.py
+Can be run either in LOCAL (do cross-validation) or submit mode.
+XGBOOST decides whether we use XGBoost gradient boosted trees.
+"""
+
+from datetime import datetime
 import pandas as pd
 import numpy as np
 import xgboost as xgb
+# We use XGBoost's version of Gradient Boosting over SkLearn's one because of
+# the approximate tree building algorithm which is much faster with nearly no
+# performance loss.
+# See Section 3 of paper https://arxiv.org/pdf/1603.02754v3.pdf
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error
-from datetime import datetime
-import csv
 
-LOCAL = True
+LOCAL = False
 XGBOOST = True
-CV = 4  # Number of folds to do on CV.
+CV = 2
+CV_METHOD = 'cv_split'
+
 
 def output_csv(pred):
-    with open('run/partial_{}.csv'.format(datetime.now()), 'w') as output:
-        writer = csv.writer(output, delimiter=';')
-        writer.writerow(['id', 'cible'])
-        writer.writerows(enumerate(pred))
+    pred = pd.Series(pred)
+    pred.index.name, pred.name = 'id', 'cible'
+    pred.to_csv('run/prediction_{}.csv'.format(datetime.now()), sep=';',
+                header=True)
+
+### Splitting functions, used for cross_val ###
+
+# FIXME: Once the cross-validation methods will be settled, use
+# sklearn's StratifiedKFold instead. For now those 3 functions are ugly.
+
 
 def split(data, before=2012):
     """
@@ -26,26 +43,29 @@ def split(data, before=2012):
     mask = ~Xtrain.columns.isin(['label'])
     return Xtrain.loc[:, mask], Xtest.loc[:, mask], ytrain, ytest
 
+
 def cv_split(data, i):
     """
-    Small wrapper around split to have it work nicely in CV.
+    Returns 3 years of train, 2 years of test.
     1) Train < 2012, Test = 2012-2013
     2) Train > 2009, Test = 2008-2009
-    ... #TODO
+    For this function we must have CV <= 2
     """
     if not i:
         return split(data)
-    elif i == 1:
+    else:
         a, b, c, d = split(data, before=2010)
         return b, a, d, c
-    return split(data)
 
-def one_vs_previous(data, i):
+
+def one_vs_previous(data, i, previous_years=2):
     """
-    Split data so that we only use the one before to predict.
+    Predict one year according to the 'previous_years' years before.
+    For this function we must have CV <= 5-previous_years
     """
-    year = data.an == i
-    previous = data.an.isin([i-1, i-2])
+    year_to_test = 2008 + i + previous_years
+    year = data.an == year_to_test
+    previous = data.an.isin(np.arange(2008 + i, year_to_test))
     Xtrain, Xtest = data[previous], data[year]
     ytrain, ytest = Xtrain.label, Xtest.label
     mask = ~Xtrain.columns.isin(['label'])
@@ -53,75 +73,87 @@ def one_vs_previous(data, i):
 
 
 def add_features(data):
+    """
+    Feature engineering other than cleaning data goes here.
+    """
     data['pourc_ald'] = data.nombre_sej_ald / data.nombre_sej
     data['diff_ald'] = data.nombre_sej - data.nombre_sej_ald
     if 'CI_AC1' in data.columns and 'CI_AC4' in data.columns:
         fillme = []
+        # Workaround to avoid division by zero.
         for beds, rea_beds in zip(data.CI_AC1, data.CI_AC4):
             fillme.append(rea_beds / beds if beds else 0)
         data['part_lits_rea'] = fillme
     return data
 
 start = datetime.now()
-
 print('Started process at {:%H:%M:%S}'.format(start))
 
-# Experiments...
+# Columns to keep. Note that keeping 40+ columns will usually swap (on 4Gb ram)
+# using Random Forest Regressors or Gradient Boosted Trees.
 to_keep = ['eta', 'nom_eta', 'prov_patient', 'dom_acti', 'age',
-        'nombre_sej_ald', 'nombre_sej', 'an', 'label']
+           'nombre_sej_ald', 'nombre_sej', 'an', 'label']
 # Extend with those that passed the treshold (> 0.1% contrib)
 to_keep.extend(['A9', 'A12', 'A13', 'A14', 'CI_AC1', 'CI_AC4',
-    'CI_AC6', 'CI_AC7', 'CI_RH4', 'CI_RH1', 'CI_RH3',
-    'CI_A5', 'CI_A12', 'CI_A15',  'P2', 'P13', 'P9',
-    'P12', 'P14', 'P15', 'A1bis', 'A2bis', 'A4bis',
-    'A5bis', 'cat'])
+                'CI_AC6', 'CI_AC7', 'CI_RH4', 'CI_RH1', 'CI_RH3',
+                'CI_A5', 'CI_A12', 'CI_A15', 'P2', 'P13', 'P9',
+                'P12', 'P14', 'P15', 'A1bis', 'A2bis', 'A4bis',
+                'A5bis', 'cat'])
 to_keep.extend(['RH{}'.format(i) for i in range(2, 6)])
+
 data = pd.read_csv('proc_data.csv', usecols=to_keep)
-data = data.fillna(-1000)  # needed to handle pdmreg and pdmza
+data.fillna(-1000, inplace=True)  # needed to handle pdmreg and pdmza
 data = add_features(data)
+
+
+### ML Tuning ###
 
 if XGBOOST:
     param = {
-            'booster': 'gbtree',
-            'max_depth': 20,
-            'objective': 'reg:linear',
-            'silent': 1,
-            'nthread': 4,
-            'eval_metric': 'rmse',
-            'seed': 1,
-            'subsample': 0.9,
-            'colsample_bytree': 0.85,
-            'colsample_bylevel': 0.9,
-            'base_score': 0.14,
-            'eta': 0.2,
+        'booster': 'gbtree',
+        'max_depth': 20,
+        'objective': 'reg:linear',
+        'silent': 1,
+        'nthread': 4,
+        'eval_metric': 'rmse',
+        'seed': 1,
+        'subsample': 0.9,
+        'colsample_bytree': 0.85,
+        'colsample_bylevel': 0.9,
+        'base_score': 0.14,
+        'eta': 0.2,
     }
 else:
-    clf = RandomForestRegressor(n_estimators=30, random_state=1, n_jobs=4, max_features='sqrt')
-    # clf = GradientBoostingRegressor(n_estimators=30, max_depth=12, max_features='sqrt',
-                                    # verbose=1, random_state=1)
+    # We don't use GBR because XGBoost is much faster and less memory-hungry.
+    clf = GradientBoostingRegressor(n_estimators=30, max_depth=12,
+                                    max_features='sqrt', verbose=1,
+                                    random_state=1)
+    clf = RandomForestRegressor(n_estimators=30, random_state=1, n_jobs=4,
+                                max_features='sqrt')
 
 if LOCAL:
     start_train = datetime.now()
     print('Started training at {:%H:%M:%S}'.format(start_train))
     scores = []
     f_i = []
-    for i in range(2010, CV+2009):
+    for i in range(CV):
         print('CV round ', i)
-        # Xtrain, Xtest, ytrain, ytest = cv_split(data, i)
-        Xtrain, Xtest, ytrain, ytest = one_vs_previous(data, i)
+        splitter = cv_split if CV_METHOD == 'cv_split' else one_vs_previous
+        Xtrain, Xtest, ytrain, ytest = splitter(data, i)
         if XGBOOST:
             xgb_train = xgb.DMatrix(Xtrain, ytrain)
             xgb_test = xgb.DMatrix(Xtest, ytest)
             eval = [(xgb_train, 'Train'), (xgb_test, 'Test')]
             clf = xgb.train(param, xgb_train, num_boost_round=80,
-                    evals=eval, verbose_eval=1, early_stopping_rounds=3)
+                            evals=eval, verbose_eval=1,
+                            early_stopping_rounds=3)
             pred = clf.predict(xgb_test)
         else:
             clf.fit(Xtrain, ytrain)
             pred = clf.predict(Xtest)
             f_i.append(clf.feature_importances_*100)
-        # With some algos, lower than 0 can be had. Correcting for it in all
-        # case. This does not improve much but every bit counts.
+        # With GradientBoostedTrees, scores can be had even though y € [0, inf[
+        # Correct this by setting negative scores to 0.
         pred = [max(0, p) for p in pred]
         scores.append(mean_squared_error(ytest, pred)**0.5*100)
 
@@ -142,17 +174,19 @@ else:
     data_wo_label = data.loc[:, data.columns != 'label']
     if XGBOOST:
         xgb_train = xgb.DMatrix(data_wo_label, data.label)
-        clf = xgb.train(param, xgb_train, num_boost_round=70)
+        clf = xgb.train(param, xgb_train, num_boost_round=1)
     else:
         clf.fit(data_wo_label, data.label)
         for col, imp in zip(data_wo_label, clf.feature_importances_*100):
             print(col, ':', imp)
     print('Training took : {} seconds'.format(datetime.now()-start_train))
 
+    # Ugly call to Garbage collector to work around ram limits.
     del data
+
+    # Run on test then output predictions.
     to_keep.remove('label')
     test = pd.read_csv('proc_test.csv', usecols=to_keep)
-    test = test[test.an == 2014]
     test = add_features(test)
     if XGBOOST:
         test = xgb.DMatrix(test)
--- transform_data.py
+++ transform_data.py
@@ -4,17 +4,19 @@ manipulate. We also join external sources (hospidiag reports) as additional
 features for our predictive model.
 """
 
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 import glob
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 sources = ['data2.csv', 'test2.csv']
+
 
 def pre_treatment():
     """
     We want to use the 'raison_sociale' field on both files to ensure classes
-    are consistent across both files. One-hot-encoding does not work here because
-    #{establishment} ~= 1300. 
+    are consistent across both files. One-hot-encoding does not work here
+    because #{establishment} ~= 1300.
     """
     names = []
     for source in sources:
@@ -24,42 +26,55 @@ def pre_treatment():
     le.fit(names)
     return le
 
+
 def generate_hospidiag(cols, exceptions):
     """
-    We want to be able to handle hospidiag data so it can be merged nicely
-    with the rest of the information we have.
-    Cols describes which columns (text) we should keep. It should be a list of lists.
-    Exceptions are columns that are not handled as numbers.
+    We want to be able to handle hospidiag data so it can be merged nicely as
+    additional features.
+    This data comes from 24 csv (3 types * 8 years of data)
+        hospidiag = Medical indicators on health establishments
+        pdmza = Market share of establishments in their activity zone
+        pdmreg = Market share of establishments in their region
+    For each file, we provide cols and exceptions:
+        Cols describes which columns (text) we should keep.
+        Exceptions are columns that are not handled as numbers.
     """
     print('Working on merger')
-    global_csv = pd.DataFrame
+    global_csv = pd.DataFrame()
     for i, kind_of in enumerate(('hospidiag', 'pdmreg', 'pdmza')):
         print('Started working on {}'.format(kind_of))
         merged_csv = pd.DataFrame()
+
         for csv in glob.glob('hospidiag/{}*.csv'.format(kind_of)):
             year = int(csv[-8:-4])
             current = pd.read_csv(csv, usecols=cols[i])
-            current = current.rename(columns = {'finess':'eta'})
+            current = current.rename(columns={'finess': 'eta'})
             # We add the year as a variable so that our merge
             # works on both eta and year.
             current['an'] = [year] * len(current)
             merged_csv = pd.concat([merged_csv, current])
-        #FIXME: If we are handling pdmza, find a better way than drop_dup
+
+        # Delete pdmza duplicates which will be troublesome when joining with
+        # pandas (duplicates create additional rows even if joined as
+        # additional columns)
         if kind_of == 'pdmza':
             merged_csv = merged_csv.drop_duplicates(['eta', 'an'])
-        # Reindex to avoid any weird interaction with multiple indexes.
-        merged_csv.index = list(range(len(merged_csv)))
-        # For all columns aside from exceptions, we want to transform it to numeric
-        # despite many variations of weird formatting
+        # Reindex to avoid issues when joining
+        merged_csv.index = np.arange(len(merged_csv))
+        # For all columns aside from exceptions, we want to transform them
+        # to numeric despite many variations of weird formatting
         for col in merged_csv.columns:
             if (col not in ('an', 'eta')) and (col not in exceptions[i]):
                 merged_csv[col] = merged_csv[col].apply(handle_ugliness)
+        # For the exceptions, we LabelEncode as {#classes} is too big to
+        # one-hot-encode them
         le = LabelEncoder()
         for col in exceptions[i]:
             merged_csv[col] = merged_csv[col].fillna('Void')
             merged_csv[col] = le.fit_transform(merged_csv[col])
+        # Variable 'zone' exists in both
         if 'zone' in merged_csv.columns:
-            merged_csv = merged_csv.rename(columns = {'zone': 'zone_{}'.format(kind_of)})
+            merged_csv = merged_csv.rename(columns={'zone': 'zone_{}'.format(kind_of)})
         print('Finished working on {}'.format(kind_of))
 
         # Handle merge logic.
@@ -67,6 +82,7 @@ def generate_hospidiag(cols, exceptions):
             global_csv = merged_csv
         else:
             global_csv = pd.merge(global_csv, merged_csv, how='left', on=['eta', 'an'])
+
         print("Shape after merge {} : {}".format(i, global_csv.shape))
     print('Finished working on merger')
     return global_csv
@@ -74,8 +90,8 @@ def generate_hospidiag(cols, exceptions):
 
 def handle_ugliness(x):
     """
-    We want to provide a nice way to convert a column to float.
-    This accounts for NaN, numeric values, and annoying , instead of .
+    Handles data format ugliness for numerical columns in data.
+    This accounts for NaN, numeric values, and annoying ',' instead of '.'
     """
     if isinstance(x, str):
         try:
@@ -84,18 +100,24 @@ def handle_ugliness(x):
             return -1
     else:
         if pd.isnull(x):
-            return -1000 # Need to be negative for e.g margin rate.
+            return -1000  # Outside of the range of each feature.
         return x
 
 cols, exceptions = [], []
 
+
+############################## COLUMN SELECTION ###########################
+###########################################################################
+####### Since we would rather not load everything in ram, only ############
+####### select columns that proved effective.                  ############
+####### Complete list of features is available on the webpage. ############
+####### Adding features here will make them available for      ############
+####### modelling.                                             ############
+###########################################################################
+
 ##### HOSPIDIAG #####
 cols_hospi = ['finess']
-# Add a bunch of numerical columns...
 cols_hospi.extend(['A9', 'A12', 'A13', 'A14'])
-# # Those proved not very useful.
-# cols_hospi.extend(['F{}_O'.format(i) for i in range(1, 13)])
-# cols_hospi.extend(['F{}_D'.format(i) for i in range(1, 13)])
 cols_hospi.extend(['P2', 'P9', 'P12', 'P13', 'P14', 'P15'])
 cols_hospi.extend(['RH{}'.format(i) for i in (2, 3, 4, 5)])
 cols_hospi.extend(['CI_AC{}'.format(i) for i in (1, 4, 6, 7)])
@@ -104,9 +126,6 @@ cols_hospi.extend(['CI_E{}'.format(i) for i in range(1, 8)])
 cols_hospi.extend(['CI_DF{}'.format(i) for i in range(1, 6)])
 cols_hospi.extend(['Q{}'.format(i) for i in range(1, 12)])
 cols_hospi.extend(['CI_E4_V2', 'CI_E7_V2'])
-# Those proved not very useful.
-# cols_hospi.extend(['CI_F{}_O'.format(i) for i in range(1, 18)])
-# cols_hospi.extend(['CI_F{}_D'.format(i) for i in range(1, 18)])
 cols_hospi.extend(['CI_RH{}'.format(i) for i in range(1, 5)])
 # The categorical ones.
 exceptions_hospi = ['cat', 'taille_MCO', 'taille_C']
@@ -126,15 +145,20 @@ cols_pdmza.extend(['A{}'.format(i) for i in range(1, 6)])
 cols.append(cols_pdmza)
 exceptions.append([])
 
+###########################################################################
+
+
 hospidiag = generate_hospidiag(cols, exceptions)
 le = pre_treatment()
 
 for source in sources:
     print('Starting to process :', source)
+    # Avoid Finess being read with several dtypes.
     data = pd.read_csv(source, sep=';', dtype={'Finess': str})
 
     is_test = 'test' in source
 
+    # Test data has one more col, id, remove it.
     if is_test:
         data.drop(['id'], axis=1, inplace=True)
 
@@ -146,7 +170,8 @@ for source in sources:
 
     data.columns = columns
 
-    # Changes that can be made before merging.
+    # Data munging and cleaning for chronic disease reports
+    # We only change eta after merging as it is one of the columns to join on.
     data.dom_acti = data.dom_acti.apply(lambda s: int(s[1:3]))
     data.prov_patient = data.prov_patient.apply(lambda s: s.replace('Inconnu', '0').replace('2A', '2000').replace('2B', '2001'))
     data.prov_patient = data.prov_patient.apply(lambda s: int(s.split('-')[0]))
@@ -157,10 +182,8 @@ for source in sources:
     data = pd.merge(data, hospidiag, how='left', on=['eta', 'an'])
     print("Shape after merging :", data.shape)
 
-    # Just manipulate eta-related columns.
-    print('Changing eta...')
+    # Changing eta
     data.eta = data.eta.apply(lambda s: int(str(s).replace('2A', '2000').replace('2B', '2001')))
-    print('eta changed...')
     data['nom_eta'] = le.transform(data.nom_eta)
 
     # Export to csv
